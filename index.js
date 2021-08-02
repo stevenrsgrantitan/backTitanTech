7d521761912dd0d3b66d847b58fc56ba1e404375
Steven Rojas <asrojasb@correo.udistrital.edu.co>
2021-08-01 23:25:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const parser = require('body-parser');
+const cors = require('cors')
+
+var api = express();
+api.use(parser.json(), cors());
+
+const port = 8001;
+
+api.get('/login', (_, res) => {
+    res.status(200);
+
+    res.send(data)
+});
+
+api.post('/Accounts/Login', (req, res) => {
+    console.log(req.body)
+    const user = req.body;
+    let response;
+    if (user.email === 'steven@gmail.com' && user.password === '123456Abc*') {
+        response = { msj: 'Bienvenido Steven', code: 200 };
+    }
+    if (user.email !== 'steven@gmail.com') {
+        response = { msj: 'Usuario no existe', code: 404 };
+    }
+    if (user.email === 'steven@gmail.com' && user.password !== '123456Abc*') {
+        response = { msj: 'Contraseña errónea', code: 400 };
+    }
+    res.status(200);
+    res.send(response)
+});
+
+
+api.listen(port, () => console.log('Servidor escuchando en puerto ' + port));
